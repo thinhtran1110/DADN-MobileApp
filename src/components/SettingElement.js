fbbf0c132ae9aab96a09dca14f7633a53509e139
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2023-04-26 04:04:30 +0700

    Finish UI Setting

---
+++ src/components/SettingElement.js
@@ -0,0 +1,19 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import color  from '../config/common/color'
+import { useNavigation } from '@react-navigation/native'
+const SettingElement = (props) => {
+    const { name, groupKey } = props;
+    const navigation = useNavigation();
+    return (
+        <View style={{alignItems: 'center'}}>
+            <View style={{width: 320, borderColor: 'rgba(0, 0, 0, 0.5)', borderBottomWidth: 1, flexDirection: 'row', marginVertical: 10}}>
+                <Text style={{width: 290, color: '#000', fontSize: 24, fontWeight: '700', fontFamily: 'Inria Serif'}}>{name}</Text>
+                <Icon name={'angle-right'} style={{ fontSize: 20, padding: 10}}></Icon>
+            </View>
+        </View>
+    )
+}
+
+export default SettingElement
